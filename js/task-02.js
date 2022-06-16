c60cc3aaf642f6cca42cc82ccda7309f95e194db
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-06-16 15:28:39 +0300

    Task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+const createItem = ingredients.map((ingredient) => `<li class = "item">${ingredient}</li>`).join('');
+listIngredients.innerHTML = createItem;
+
+
+console.log(listIngredients);
